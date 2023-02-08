0561cd1faffa1d1a9a917711ffbb0e79370ffa1b
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2023-02-08 15:49:00 +0700

    change fetch url

--- Frontend/src/services/Pulldata.js
+++ Frontend/src/services/Pulldata.js
@@ -1,21 +1,21 @@
 import axios from "axios"
 
 export async function GetData() {
-    const res = await axios.get(`http://group9.exceed19.online/rooms/front`)
+    const res = await axios.get(`http://group9.exceed19.online/exceed09/rooms/front`)
     return res.data
 }
 
 export async function SetSwitch(id, status) {
-    const res = await axios.put(`http://group9.exceed19.online/switch/${id}/${status}`)
+    const res = await axios.put(`http://group9.exceed19.online/exceed09/switch/${id}/${status}`)
     return res.data
 }
 
 export async function SetMode(id, status) {
-    const res = await axios.put(`http://group9.exceed19.online/mode/${id}/${status}`)
+    const res = await axios.put(`http://group9.exceed19.online/exceed09/mode/${id}/${status}`)
     return res.data
 }
 
 export async function SetBrightness(id, status) {
-    const res = await axios.put(`http://group9.exceed19.online/brightness/${id}/${status}`)
+    const res = await axios.put(`http://group9.exceed19.online/exceed09/brightness/${id}/${status}`)
     return res.data
 }
