5d566e659dcbcd513b7c677c460846f3e5b6ba13
Tharunthorn Musik <tharunthornmusik@Tharunthorns-MacBook-Air.local>
2023-02-08 13:21:06 +0700

    fixbug

--- Backend/router/brightness.py
+++ Backend/router/brightness.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import os
 import urllib
 
-load_dotenv('/Users/tharunthornmusik/Documents/CodeSpace/KU/ExceedCamp/Mongo/.env')
+load_dotenv('.env')
 
 user = os.getenv('username')
 password =  os.getenv('password')
@@ -25,10 +25,18 @@ def get_brightness_change(room_id : int, brightness : int):
     if not (room_id < 1 or room_id > 3) and not (brightness < 1 or brightness > 255):
         collection.update_one({"room_id":room_id}, {"$set":{"brightness":brightness, "is_change": True}})
         room = list(collection.find({"room_id": room_id}))[0]
-        return {"room_id": room["room_id"], "brightness": room["brightness"], "is_change": room["is_change"]}
+        result = list()
+        for room in collection.find({}):
+            result.append({
+                "room_id": room["room_id"],
+                "mode_auto": room["mode_auto"],
+                "on_status": room["on_status"],
+                "brightness": room["brightness"],
+            })
+        return {"message": result} 
     if room_id < 1 or room_id > 3:
-        return HTTPException(status_code=404, detail="Room not found")
+        raise HTTPException(status_code=404, detail="Room not found")
     if brightness < 1 or brightness > 255:
-        return HTTPException(status_code=404, detail="Brightness invalid")
+        raise HTTPException(status_code=404, detail="Brightness invalid")
     else:
-        return HTTPException(status_code=400, detail="Bad request")
+        raise HTTPException(status_code=400, detail="Bad request")
